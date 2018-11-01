31703b9d6583c094b0b92625e8a2fcf25bf29a73
Pablo Alejandro Bertoli <pbertoli@alumno.unlam.edu.ar>
2018-11-01 20:46:33 -0300

    Untracked files issue resolved to fix .gitignore

---
+++ TrabajoPracticoPw3/TrabajoPracticoPw3/Controllers/PedidosController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrabajoPracticoPw3.Controllers
+{
+    public class PedidosController : Controller
+    {
+        // GET: Pedidos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Iniciar()
+        {
+            return View();
+        }
+
+        public ActionResult Iniciar(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Iniciado(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Lista()
+        {
+            return View();
+        }
+
+        public ActionResult Editar()
+        {
+            return View();
+        }
+
+        public ActionResult Eliminar()
+        {
+            return View();
+        }
+
+        public ActionResult Elegir()
+        {
+            return View();
+        }
+
+        public ActionResult Detalle()
+        {
+            return View();
+        }
+
+    }
+}
