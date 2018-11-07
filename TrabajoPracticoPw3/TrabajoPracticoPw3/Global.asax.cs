8fc0f621324c3cb2ff0124ee5b60ed841028f209
Pablo Alejandro Bertoli <pbertoli@alumno.unlam.edu.ar>
2018-11-07 20:58:35 -0300

    Utilizacion de Bundles

--- TrabajoPracticoPw3/TrabajoPracticoPw3/Global.asax.cs
+++ TrabajoPracticoPw3/TrabajoPracticoPw3/Global.asax.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+using TrabajoPracticoPw3.App_Start;
 
 namespace TrabajoPracticoPw3
 {
@@ -13,6 +15,7 @@ namespace TrabajoPracticoPw3
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
