d99a843b3493fab2bb76680a5a1528d34a294186
Pablo Alejandro Bertoli <pbertoli@alumno.unlam.edu.ar>
2018-11-08 15:16:08 -0300

    Implementacion de bundles

--- TrabajoPracticoPw3/TrabajoPracticoPw3/App_Start/BundleConfig.cs
+++ TrabajoPracticoPw3/TrabajoPracticoPw3/App_Start/BundleConfig.cs
@@ -8,7 +8,6 @@ namespace TrabajoPracticoPw3.App_Start
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -16,19 +15,33 @@ namespace TrabajoPracticoPw3.App_Start
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                        "~/Scripts/bootstrap.min.js",
+                        "~/Scripts/bootstrap.bundle.min.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/Site").Include(
+                "~/Content/Site.css",
+                "~/Content/bootstrap.min.css",
+                "~/Content/templateEmpanada/css/half-slider.css"
+                ));
+
+            //bundles.Add(new ScriptBundle("~/bundles/Site").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/respond.js"));
+            
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/respond.js"));
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
