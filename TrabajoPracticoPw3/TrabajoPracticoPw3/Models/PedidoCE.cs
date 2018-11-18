57435309135c6ac990da88450c58a35a2fa67c0d
rigaudi <rigaudi@hotmail.com>
2018-11-18 17:05:35 -0300

    Se agregan clases para las validaciones

---
+++ TrabajoPracticoPw3/TrabajoPracticoPw3/Models/PedidoCE.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrabajoPracticoPw3.Models
+{
+    public class PedidoCE
+    {
+        [StringLength(200), Required(ErrorMessage = "El Nombre es obligatorio y hasta 200 caracteres")]
+        public string NombreNegocio { get; set; }
+        //[Required(ErrorMessage ="Agregar una descripción")]
+        public string Descripcion { get; set; }
+        [Range(0, 9999), Required(ErrorMessage = "El Precio es obligatorio y hasta 4 caracteres")]
+        public int PrecioUnidad { get; set; }
+        [Range(0, 9999), Required(ErrorMessage = "El Precio es obligatorio y hasta 4 caracteres")]
+        public int PrecioDocena { get; set; }
+    }
+
+    [MetadataType(typeof(PedidoCE))]
+
+    public partial class Pedido
+    {
+
+    }
+}
---
+++ TrabajoPracticoPw3/TrabajoPracticoPw3/Models/UsuarioCE.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrabajoPracticoPw3.Models
+{
+    public class UsuarioCE
+    {   
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+
+    [MetadataType(typeof(UsuarioCE))]
+
+    public partial class Usuario
+    {
+
+    }
+}
